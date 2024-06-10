5d70f4490cc25753abfac57336c3abf5528937a1
Dark Fated <darkfated@icloud.com>
2024-06-11 02:37:43 +0300

    Debug off

--- lua/mantle_addons/itemrestrictions/client.lua
+++ lua/mantle_addons/itemrestrictions/client.lua
@@ -1,7 +1,6 @@
 hook.Add('PostReloadToolsMenu', 'ItemRestrictions.RemoveTools', function()
     for _, v in pairs(g_SpawnMenu.ToolMenu.ToolPanels[1].List.pnlCanvas:GetChildren()) do
         for _, item in pairs(v:GetChildren()) do
-            print(item.Name)
             if item.Name and ItemRestrictions.cfg.hide_tools[item.Name] then
                 item:Remove()
             end
